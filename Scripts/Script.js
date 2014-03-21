151cbbd5c8162180357c0681c6dace25bfc733aa
David Gardner <david_lng8@yahoo.com>
2014-03-21 06:06:19 -0500

    Uploading

---
+++ Scripts/Script.js
@@ -0,0 +1,8 @@
+﻿var backdrop = document.getElementById("shadow");
+//var box = document.getElementById("box");
+
+function register() {
+    alert(backdrop == null);
+    //backdrop.style.display("block");
+    //box.style.display("block");
+}
